560e6bc87cb3ae2484a6ac6af5b9bd48ca7d6f19
Olivier Nicole <nicole@ensta.fr>
2016-05-05 17:16:24 +0100

    Start writing module Translstatic (unused for now)

---
+++ bytecomp/translstatic.ml
@@ -0,0 +1,18 @@
+open Typedtree
+open Lambda
+
+let transl_implementation module_name str (* module coercion unhandled *) =
+  transl_structure str.str_items
+
+let rec transl_structure = function
+  | [] -> lambda_unit
+  | item :: rem ->
+    begin
+      match item.str_desc with
+      | Tstr_value(Static, rec_flag, pat_expr_list) ->
+          let body = transl_structure rem in
+          transl_let rec_flag pat_expr_list body
+      | _ -> (* all non-static bindings are ignored *)
+          transl_structure rem
+    end
+
---
+++ bytecomp/translstatic.mli
@@ -0,0 +1,25 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*               Olivier Nicole, Paris-Saclay University                  *)
+(*                                                                        *)
+(*                       Copyright 2016 OCamlLabs                         *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** translstatic.ml: Translation of static declarations to lambda terms for the
+    module language. *)
+
+open Typedtree
+open Lambda
+
+(** Translates to lambda terms all static bindings (i.e. bindings declared with
+    the [static] keyword), ignore other declarations. *)
+(* module coercion not handled *)
+val transl_implementation : string -> structure -> lambda
+
