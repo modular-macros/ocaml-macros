2a621b598a4576f7ec331fac1d04d9ea099100fb
Olivier Nicole <nicole@ensta.fr>
2016-05-13 18:17:34 +0100

    Revert "Fix bug in `.cmt` file saving"
    This reverts commit 405b41ff918b6af18ec60f8d94086942b614b984.
    See previous revert message.

--- ocamldoc/odoc_analyse.ml
+++ ocamldoc/odoc_analyse.ml
@@ -83,7 +83,6 @@ let process_implementation_file sourcefile =
   let modulename = String.capitalize_ascii(Filename.basename prefixname) in
   Env.set_unit_name modulename;
   let inputfile = preprocess sourcefile in
-  let stat_env = initial_env () in
   let env = initial_env () in
   try
     let parsetree =
@@ -92,7 +91,7 @@ let process_implementation_file sourcefile =
     in
     let typedtree =
       Typemod.type_implementation
-        sourcefile prefixname modulename stat_env env parsetree
+        sourcefile prefixname modulename env parsetree
     in
     (Some (parsetree, typedtree), inputfile)
   with
