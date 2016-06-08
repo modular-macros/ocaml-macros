937f7892e1dee4918d8db1490c6e4c8a40b1932b
Olivier Nicole <nicole@ensta.fr>
2016-06-08 15:20:08 +0100

    Forgotten to add the `Runstatic` module to the repo

---
+++ bytecomp/runstatic.ml
@@ -0,0 +1,34 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*               Olivier Nicole, Paris-Saclay University                  *)
+(*                                                                        *)
+(*                      Copyright 2016 OCaml Labs                         *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+let run_static ppf lam =
+  let (init_code, fun_code) = Bytegen.compile_phrase lam in
+  let (code, code_size, reloc, _) =
+    Emitcode.to_memory init_code fun_code
+  in
+  ignore (Symtable.init_toplevel ());
+  Symtable.init_static ();
+  Cmo_load.load_deps ppf Asttypes.Static reloc
+    (fun () -> ())
+    (fun () -> ())
+    (fun exn -> raise exn);
+  Symtable.patch_object code reloc;
+  Symtable.check_global_initialized reloc;
+  Symtable.update_global_table ();
+  Printf.fprintf stderr "before reify\n%!";
+  let splices = (Meta.reify_bytecode code code_size) () in
+  Printf.fprintf stderr "after reify\n%!";
+  Symtable.reset ();
+  (Obj.obj splices : Parsetree.expression array)
+
---
+++ bytecomp/runstatic.mli
@@ -0,0 +1,22 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*               Olivier Nicole, Paris-Saclay University                  *)
+(*                                                                        *)
+(*                      Copyright 2016 OCaml Labs                         *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** runstatic.ml: Execution of static terms and splices. *)
+
+open Lambda
+
+(** Run the static code for an implementation and return the array of splices.
+    *)
+val run_static : Format.formatter -> lambda -> Parsetree.expression array
+
