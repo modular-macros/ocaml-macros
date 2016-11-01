08977ac519871f85f712b1f18e0faaec45a4382f
Olivier Nicole <nicole@ensta.fr>
2016-11-01 14:42:37 +0000

    Add test for protection of global constructors against shadowing

--- testsuite/tests/macros/splicing_shadowed_global.ml
+++ testsuite/tests/macros/splicing_shadowed_global.ml
@@ -1,7 +1,18 @@
-static f () = << List.map Pervasives.pred [1;2;3] >>;;
+static x = << List.map Pervasives.pred [1;2;3] >>;;
+static y = << Some 42 >>;;
 
 module Pervasives = struct
   let pred = Pervasives.succ
 end;;
 
-let l = $(f ());;
+type fake_option =
+  | Blah
+  | Some of int
+;;
+
+let l = $x;;
+let b =
+  match $y with
+  | Blah -> false
+  | Some x -> x = 42
+;;
