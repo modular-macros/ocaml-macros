7ce72525dd10e88114c8cc81a26307c693073f88
Olivier Nicole <nicole@ensta.fr>
2016-11-18 16:35:57 +0000

    Turn `Expr` functions to macros and bootstrap

--- stdlib/expr.ml
+++ stdlib/expr.ml
@@ -1,28 +1,28 @@
 open ^CamlinternalQuote
 open ^Pervasives
 
-static of_bool =
+macro of_bool =
   function
   | false -> <<false>>
   | true -> <<true>>
 
-static of_int i =
+macro of_int i =
   ast_to_expr @@ Exp.constant (Loc.none) @@
   Constant.integer i
 
-static of_float f =
+macro of_float f =
   ast_to_expr @@ Exp.constant (Loc.none) @@
   Constant.floating f
 
-static of_char c =
+macro of_char c =
   ast_to_expr @@ Exp.constant (Loc.none) @@
   Constant.char c
 
-static of_string s =
+macro of_string s =
   ast_to_expr @@ Exp.constant (Loc.none) @@
   Constant.string s
 
-static rec of_list f = function
+macro rec of_list f = function
   | [] -> << [] >>
   | x :: xs -> << $(f x) :: $(of_list f xs) >>
 
--- stdlib/expr.mli
+++ stdlib/expr.mli
@@ -1,7 +1,7 @@
-static val of_bool : bool -> bool expr
-static val of_int : int -> int expr
-static val of_float : float -> float expr
-static val of_char : char -> char expr
-static val of_string : string -> string expr
-static val of_list : ('a -> 'a expr) -> 'a list -> 'a list expr
+macro of_bool : bool -> bool expr
+macro of_int : int -> int expr
+macro of_float : float -> float expr
+macro of_char : char -> char expr
+macro of_string : string -> string expr
+macro of_list : ('a -> 'a expr) -> 'a list -> 'a list expr
 
