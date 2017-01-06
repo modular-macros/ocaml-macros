2fc27a1cb723951b8604e17c0df99c1f01d6b114
Olivier Nicole <nicole@ensta.fr>
2017-01-06 15:27:13 +0000

    Add CamlinternalLambda

---
+++ stdlib/camlinternalLambda.mli
@@ -0,0 +1,76 @@
+(** lambda type. Similar to (and trivially transformable into) [Lambda.t]. *)
+
+module Ident : sig
+  type t = string
+end
+
+module Location : sig
+  type t
+end
+
+type primitive
+
+type value_kind
+
+type direction_flag
+
+type structured_constant
+
+type function_kind
+
+type inline_attribute
+
+type specialise_attribute
+
+type let_kind
+
+type meth_kind
+
+type function_attribute
+
+type lambda_event
+
+type lambda =
+    Lvar of Ident.t
+  | Lconst of structured_constant
+  | Lapply of lambda_apply
+  | Lfunction of lfunction
+  | Llet of let_kind * value_kind * Ident.t * lambda * lambda
+  | Lletrec of primitive * lambda list * Location.t
+  | Lprim of primitive * lambda list * Location.t
+  | Lswitch of lambda * lambda_switch
+  | Lstringswitch of
+      lambda * (string * lambda) list * lambda option * Location.t
+  | Lstaticraise of int * lambda list
+  | Lstaticcatch of lambda * (int * Ident.t list) * lambda
+  | Ltrywith of lambda * Ident.t * lambda
+  | Lifthenelse of lambda * lambda * lambda
+  | Lsequence of lambda * lambda
+  | Lwhile of lambda * lambda
+  | Lfor of Ident.t * lambda * lambda * direction_flag * lambda
+  | Lassign of Ident.t * lambda
+  | Lsend of meth_kind * lambda * lambda * lambda list * Location.t
+  | Levent of lambda * lambda_event
+  | Lifused of Ident.t * lambda
+
+and lfunction =
+  { kind : function_kind;
+    params : Ident.t list;
+    body : lambda;
+    attr : function_attribute;
+    loc : Location.t }
+
+and lambda_apply =
+  { ap_func : lambda;
+    ap_args : lambda list;
+    ap_loc : Location.t;
+    ap_should_be_tailcall : bool;
+    ap_inlined : inline_attribute;
+    ap_specialised : specialise_attribute; }
+
+and lambda_switch =
+  { sw_numconsts: int;                  (* Number of integer cases *)
+    sw_consts: (int * lambda) list;     (* Integer cases *)
+    sw_numblocks: int;                  (* Number of tag block cases *)
+    sw_blocks: (int * lambda) list;     (* Tag block cases *)
+    sw_failaction : lambda option}      (* Action to take if failure *)
