f8d47012058f7f007d86e54f0d0f1edb2f5a99f9
Jeremy Yallop <yallop@gmail.com>
2024-08-23 08:39:30 +0100

    MacoCaml: add some more tests from the bugs suite.

--- testsuite/tests/macros/gadts.ml
+++ testsuite/tests/macros/gadts.ml
@@ -9,7 +9,7 @@ macro m : type a. (a, int) Type.eq expr -> a -> unit expr =
 
 [%%expect{|
 File "_none_", line 1:
-Warning 75 [maco-dev]: maco: avoided gadt constraint at level 0
+Warning 76 [maco-dev]: maco: avoided gadt constraint at level 0
 
 Line 3, characters 47-48:
 3 |   << match $p with Type.Equal -> $(let _ = 1 + x in << () >>) >>
@@ -25,7 +25,7 @@ macro m : type a. (a, int) Type.eq expr -> unit expr =
 
 [%%expect{|
 File "_none_", line 1:
-Warning 75 [maco-dev]: maco: avoided gadt constraint at level 0
+Warning 76 [maco-dev]: maco: avoided gadt constraint at level 0
 
 macro m : ('a, int) Type.eq expr -> unit expr = <fun>
 |}]
@@ -38,7 +38,7 @@ macro m : type a. (a, int) Type.eq expr -> a expr -> int expr =
 
 [%%expect{|
 File "_none_", line 1:
-Warning 75 [maco-dev]: maco: avoided gadt constraint at level 0
+Warning 76 [maco-dev]: maco: avoided gadt constraint at level 0
 
 Line 3, characters 34-35:
 3 |   << match $p with Type.Equal -> $x >>
