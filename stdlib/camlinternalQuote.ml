54bb8a9ee69c72b0f20a41f20615b4f25a6c4c2c
michaelleejl <michaelleejl@gmail.com>
2025-06-05 12:30:14 +0100

    Added helper functions for scope extrusion checking to camlinternalQuote

--- stdlib/camlinternalQuote.ml
+++ stdlib/camlinternalQuote.ml
@@ -1,5 +1,12 @@
 module Identifier =
 struct
+  open Effect 
+  open Effect.Deep 
+  module IdentSet = Set.Make(CamlinternalLam.Ident)
+  type set_of_t = Set.Make(CamlinternalLam.Ident).t
+
+  type _ Effect.t += FreeVar: IdentSet.t -> unit Effect.t
+
   type t = CamlinternalLam.Ident.t
 
   (* Oleg points out an issue that we need to take care about:
@@ -18,4 +25,26 @@ struct
      Local { name; stamp = !currentstamp }
   | Global name | Predef { name ; _ } ->
      failwith ("CamlinternalQuote.rename " ^ name)
+
+  let exec_in_current_scope scope f = match f () with 
+      effect FreeVar fvs, k-> let free = IdentSet.diff fvs scope in 
+                             Printf.printf "AST has %d free variables \n" (IdentSet.cardinal fvs);
+                             Printf.printf "scope has %d bound variables \n " (IdentSet.cardinal scope);
+                             Printf.printf "There are %d free variables \n " (IdentSet.cardinal (IdentSet.diff fvs scope));
+                             (if not (IdentSet.is_empty free) then perform (FreeVar free));
+                             continue k ()
+      | c, fvs -> (c, IdentSet.diff fvs scope)
+
+   let new_scope alphas f name = let scope = Printf.printf "New scope: %s \n" name; IdentSet.of_list alphas in 
+                                             Printf.printf "Added scope: %d bound variables \n" (IdentSet.cardinal scope);
+                                             exec_in_current_scope scope f
+                                             
+  let empty = fun () -> IdentSet.empty
+  let free_var var = IdentSet.singleton var
+  let merge_free_vars fvs1 fvs2 = IdentSet.union fvs1 fvs2
+  let check vars name =  Printf.printf "Checking: %s \n" name; if not (IdentSet.is_empty vars) then Printf.printf "called with %d free variables \n" (IdentSet.cardinal vars); perform (FreeVar vars)
+
+  let scope_extrusion_check f = try f () with 
+      effect FreeVar _, _ -> failwith "Scope Extrusion Check"
+
 end
--- stdlib/camlinternalQuote.mli
+++ stdlib/camlinternalQuote.mli
@@ -1,5 +1,16 @@
 module Identifier : sig
   type t
 
+  type set_of_t = Set.Make(CamlinternalLam.Ident).t
   val rename : t -> t
-end
+
+  val new_scope : t list -> (unit -> 'a * set_of_t) -> string -> 'a * set_of_t
+
+  val empty : unit -> set_of_t
+  val free_var : t -> set_of_t
+  val merge_free_vars: set_of_t -> set_of_t -> set_of_t
+  val check : set_of_t -> string -> unit 
+
+  val scope_extrusion_check: (unit -> 'a * set_of_t) -> 'a * set_of_t
+
+  end
