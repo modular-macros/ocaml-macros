8b9c55bf967ca9f20adb89da9dad540cedc53f91
Michael Lee <64633761+michaelleejl@users.noreply.github.com>
2025-10-13 20:39:56 +0800

    Removed warning for levelled GADTs (#28)
    Removed warning for levelled GADTs

--- testsuite/tests/macros/gadts.ml
+++ testsuite/tests/macros/gadts.ml
@@ -8,27 +8,21 @@ macro m : type a. (a, int) Type.eq expr -> a -> unit expr =
 ;;
 
 [%%expect{|
-File "_none_", line 1:
-Warning 75 [maco-dev]: maco: avoided gadt constraint at level 0
-
 Line 3, characters 47-48:
 3 |   << match $p with Type.Equal -> $(let _ = 1 + x in << () >>) >>
                                                    ^
 Error: The value "x" has type "a" but an expression was expected of type "int"
 |}]
 
-(* TODO: we don't need a warning here *)
 macro m : type a. (a, int) Type.eq expr -> unit expr =
   fun p ->
   << match $p with Type.Equal -> () >>
 ;;
 
 [%%expect{|
-File "_none_", line 1:
-Warning 75 [maco-dev]: maco: avoided gadt constraint at level 0
-
 macro m : ('a, int) Type.eq expr -> unit expr = <fun>
 |}]
+
 
 (* TODO: we should eventually allow this *)
 macro m : type a. (a, int) Type.eq expr -> a expr -> int expr =
@@ -37,9 +31,6 @@ macro m : type a. (a, int) Type.eq expr -> a expr -> int expr =
 ;;
 
 [%%expect{|
-File "_none_", line 1:
-Warning 75 [maco-dev]: maco: avoided gadt constraint at level 0
-
 Line 3, characters 34-35:
 3 |   << match $p with Type.Equal -> $x >>
                                       ^
